97c356dc993fe8da8bc861765c42b09aa4611283
anupamdhauria <anupamdhauria97@gmail.com>
2025-06-12 17:30:17 +0530

    bean scope ID

---
+++ IOCProject-10-SpringBeanID/src/in/ineuron/test/TestApp.java
@@ -0,0 +1,35 @@
+package in.ineuron.test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.ineuron.comp.Student;
+
+public class TestApp {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("***********ApplicationContext container starting************");
+		ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext(
+				"in/ineuron/cfg/applicationContext.xml");
+		System.out.println("***********ApplicationContext container started*************");
+
+		System.out.println("*********Container started**********");
+		System.out.println("Bean id is :: " + Arrays.toString(factory.getBeanDefinitionNames()));
+
+		System.in.read();
+
+		Student stud1 = factory.getBean("in.ineuron.comp.Student#0", Student.class);
+		System.out.println(stud1);
+
+		Student stud2 = factory.getBean("in.ineuron.comp.Student#1", Student.class);
+		System.out.println(stud2);
+
+		factory.close();
+		System.out.println("\n*********Container stopped**********");
+
+	}
+
+}
