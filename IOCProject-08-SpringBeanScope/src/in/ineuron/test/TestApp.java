a9ce22b828a37e0700389da7257b528dfb6f0297
anupamdhauria <anupamdhauria97@gmail.com>
2025-06-12 17:12:31 +0530

    Bean Scope

---
+++ IOCProject-08-SpringBeanScope/src/in/ineuron/test/TestApp.java
@@ -0,0 +1,39 @@
+package in.ineuron.test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+import in.ineuron.comp.WishMessageGenerator;
+
+public class TestApp {
+
+	public static void main(String[] args) throws IOException {
+		
+
+		System.out.println("***********ApplicationContext container starting************");
+		ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("in/ineuron/cfg/applicationContext.xml");
+		System.out.println("***********ApplicationContext container started*************");
+
+		System.in.read();
+		WishMessageGenerator wm = (WishMessageGenerator)factory.getBean("wmg",WishMessageGenerator.class);
+		System.out.println("HasCode for wm is: "+wm.hashCode());
+		
+		System.out.println();
+		
+		WishMessageGenerator wm1 = (WishMessageGenerator)factory.getBean("wmg",WishMessageGenerator.class);
+		System.out.println("HasCode for wm1 is: "+wm1.hashCode());
+	
+		System.out.println();
+
+		System.out.println("*******Container is closing**********");
+		
+		
+	
+		
+	}
+
+}
